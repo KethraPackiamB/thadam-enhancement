888feef79d863f99734a362a78fffe519a4407ba
KethraPackiamB <kethrapackiamb@gmail.com>
2026-03-03 21:36:04 +0530

    Connected MongoDB Database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express()
+
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(() => console.log("DB Error"));
 
 app.get('/', (req, res) =>{
     res.send("Server Started")
@@ -7,6 +16,6 @@ app.get('/', (req, res) =>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
