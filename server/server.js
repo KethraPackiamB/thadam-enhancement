776a15c1285a9c0144b07042bddb965356eb2727
pavi-191004 <bpavithra4646@gmail.com>
2026-03-08 14:38:10 +0530

    Added delete customer API

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
 require("dotenv").config();
+
+const customerRoutes = require("./routes/customerRoutes");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URL)
+// MongoDB Connection
+mongoose
+  .connect(process.env.DB_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(() => console.log("DB Error"));
 
@@ -16,8 +20,11 @@ app.get("/", (req, res) => {
   res.send("Server Started");
 });
 
+// Base API URL
+app.use("/api", customerRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
