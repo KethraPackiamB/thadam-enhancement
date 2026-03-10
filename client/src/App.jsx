a92e57d24a3026e856b3075aff15a9db3280d7fd
pavi-191004 <bpavithra4646@gmail.com>
2026-03-10 10:07:20 +0530

    resolve the conflict in main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,38 +6,28 @@ import CustomerTable from "./components/table/CustomerTable";
 import { CustomerContextProvider } from "./context/CustomerTableContext";
 
 import WelcomeMessage from "./pages/Dashboard/WelcomeMessage";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import AppRoutes from "./routes/AppRoutes";
-
 
 function App() {
   return (
-<>
-<div>
-    <WelcomeMessageContext>
-      <DashboardLayout />
-      <WelcomeMessage />
-    </WelcomeMessageContext>
-     <CustomerContextProvider>
-        <CustomerTable/>
-    </CustomerContextProvider>
-    
-    
-    {/* <WelcomeMessageContext>
-      <DashboardLayout />
-      <WelcomeMessage />
-    </WelcomeMessageContext> */}
-   <BrowserRouter>
-   <AppRoutes/>
-   </BrowserRouter>
-    </div>
+    <>
+      <div>
+        <WelcomeMessageContext>
+          <DashboardLayout />
+          <WelcomeMessage />
+        </WelcomeMessageContext>
+        <CustomerContextProvider>
+          <CustomerTable />
+        </CustomerContextProvider>
 
-</>
-    
-   
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
 export default App;
-
