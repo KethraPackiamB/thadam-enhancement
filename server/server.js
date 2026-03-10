b7d6f02c8d3db692eecac2667373659460d82c49
pavi-191004 <bpavithra4646@gmail.com>
2026-03-10 10:07:20 +0530

    resolve the conflict in main

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const customerRoutes = require("./Routes/customerRoutes");
+const customerRoutes = require("./routes/customerRoutes");
 
 const app = express();
 
