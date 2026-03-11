d02f68e1f302062b6481f1640fe8c518daccfe36
KethraPackiamB <kethrapackiamb@gmail.com>
2026-03-11 12:52:26 +0530

    added routing to add customer form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,31 @@
-// import {BrowserRouter} from "react-router-dom"
-// import { WelcomeMessageContext } from "./context/WelcomeMessageContext";
-// import DashboardLayout from "./layout/DashboardLayout";
+import { BrowserRouter } from "react-router-dom";
+import { WelcomeMessageContext } from "./context/WelcomeMessageContext";
+import DashboardLayout from "./layout/DashboardLayout";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CustomerFormPage from "./pages/CustomerFormPage/CustomerFormPage";
 
-// import CustomerTable from "./components/table/CustomerTable";
-// import { CustomerContextProvider } from "./context/CustomerTableContext";
+import CustomerTable from "./components/table/CustomerTable";
+import { CustomerContextProvider } from "./context/CustomerTableContext";
 
-// import WelcomeMessage from "./pages/Dashboard/WelcomeMessage";
-// import "bootstrap-icons/font/bootstrap-icons.css";
-// import AppRoutes from "./routes/AppRoutes";
+import WelcomeMessage from "./pages/Dashboard/WelcomeMessage";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
       <div>
-        {/* <WelcomeMessageContext>
-          <DashboardLayout />
-          <WelcomeMessage />
-        </WelcomeMessageContext>
-        <CustomerContextProvider>
-          <CustomerTable />
-        </CustomerContextProvider>
-
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter> */}
-        <CustomerFormPage/>
+       
+          <WelcomeMessageContext>
+            <CustomerContextProvider>
+              {/* <DashboardLayout />
+              <WelcomeMessage /> */}
+               <AppRoutes/>
+            </CustomerContextProvider>
+          </WelcomeMessageContext>
+       
+      
       </div>
     </>
   );
