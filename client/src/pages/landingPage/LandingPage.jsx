3c2c5ae1d3c150a0b56e261642267d357c965fb1
pavi-191004 <bpavithra4646@gmail.com>
2026-03-15 18:20:02 +0530

    Refactor API URLs to use environment variable and update LandingPage component

--- client/src/pages/landingPage/LandingPage.jsx
+++ client/src/pages/landingPage/LandingPage.jsx
@@ -1,11 +1,37 @@
- export const LandingPage=()=>{
-    const handleLogin = () => {
-    window.location.href = "https://thadam-bsba.onrender.com/login";
-  };
-    return(
- <button onClick={handleLogin}>
-      Login with WorkOS
-    </button>
-    )
+import React from "react";
+import thadam from "../../assets/thadamLogo.svg";
+const BACKEND_LIVE_BASE_URL= import.meta.env.BACKEND_LIVE_BASE_URL;
+export const LandingPage = () => {
 
-}
+  const handleLogin = () => {
+    window.location.href = `${BACKEND_LIVE_BASE_URL}/login"`;
+  };
+
+  return (
+    <div className="container-fluid vh-100 d-flex justify-content-center align-items-center bg-light">
+
+      <div className="card shadow-lg p-4 text-center" style={{ width: "350px" }}>
+
+        <img
+          src={thadam}
+          alt="logo"
+          className="mb-3 mx-auto"
+         
+        />
+
+        <p className="text-muted mb-4">
+          Manage and track your customer details efficiently in one place.
+        </p>
+
+        <button
+          className="btn btn-primary btn-lg w-100"
+          onClick={handleLogin}
+        >
+          Get Started
+        </button>
+
+      </div>
+
+    </div>
+  );
+};
