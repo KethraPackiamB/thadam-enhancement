76bc906a803b3a6593ded857d3ccf64103ed4533
pavi-191004 <bpavithra4646@gmail.com>
2026-03-31 13:34:52 +0530

    implement user profile end-to-end (DB, API, frontend integration)

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes=require("./routes/userRoutes")
 const app = express();
 
-app.use(cors({
-  origin:'http://localhost:5173'
-}));
+app.use(cors());
 app.use(express.json());
 
 mongoose
